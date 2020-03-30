7eaa4d9ef69ffae29ed7add48d31a2b26cc82593
Paul K <pkugelmass@gmail.com>
2020-03-29 23:13:40 -0400

    Improved Event model and TribalReward. Other events broken now, though.

--- events/challenges.py
+++ events/challenges.py
@@ -1,13 +1,67 @@
 from .events import Event
 from .tribal import TribalCouncil
+from numpy.random import sample, choice
+from numpy import mean
 
 class Challenge(Event):
     def __init__(self,day,**kwargs):
         super().__init__(day,**kwargs)
         self.time = 12
 
-class IndividualMixin():
+    def run_challenge(self):
+        self.result = choice(self.participants)
 
+class TribalMixin:
+
+    def equalize_tribes(self):
+        bench = {tribe:[] for tribe in self.participants}
+        sizes = [len(x.players) for x in self.participants]
+        if max(sizes) != min(sizes):
+            for tribe in self.participants:
+                sit_out = len(x.players) - min(sizes)
+                if sit_out > 0:
+                    self.record('{} has to sit out {} players.'.format(tribe,sit_out))
+                    sit_outs = sample(tribe.players,sit_out)
+                    bench[tribe] = sit_outs
+                    self.record('{} will sit out for {}; take a seat on the bench.'.format(sit_outs,tribe))
+        return bench
+
+    def calculate_strength(self, bench):
+        tribestrengths = []
+        for tribe in self.participants:
+            indstrength = [p.physical for p in tribe.players if p not in bench[tribe]]
+            strength = mean(indstrength)
+            tribestrengths.append(strength)
+        relative = [float(x)/sum(tribestrengths) for x in tribestrengths]
+        strengths = dict(zip(self.participants,relative))
+        self.record('Relative strengths: {}'.format(strengths))
+        return strengths
+
+    def run_challenge(self,strength):
+        self.result = choice(list(strength.keys()),p=list(strength.values()))
+
+
+class TribalReward(TribalMixin, Challenge):
+    def __init__(self,day,**kwargs):
+        super().__init__(day,**kwargs)
+        self.name = 'Tribal Reward'
+
+    def run(self,game):
+        self.participants = game.tribes
+        self.record('You ready for a reward challenge?')
+        self.record('Getting a look at our tribes, {}.'.format(self.participants))
+        sit_outs = self.equalize_tribes() #TribalMixin
+        strengths = self.calculate_strength(sit_outs) #TribalMixin
+        self.run_challenge(strengths) #TribalMixin
+        self.announce_winner() #TribalReward
+        self.mark_complete() #Event
+
+    def announce_winner(self):
+        losers = [x for x in self.participants if self.result != x]
+        self.record('{} wins reward!'.format(self.result))
+        self.record('{}, go back to camp; got nothin\' for ya.'.format(losers))
+
+class IndividualMixin():
     def find_participants(self,game):
         self.who = game.active_players()
 
@@ -32,13 +86,7 @@ class IndividualReward(IndividualMixin, Challenge):
         super().__init__(day,**kwargs)
         self.name = 'Individual Reward'
 
-
-
-class TribesMixin:
-    def find_participants(self,game):
-        self.who = game.tribes
-
-class TribalImmunity(TribesMixin,Challenge):
+class TribalImmunity(Challenge):
     def __init__(self,day,**kwargs):
         super().__init__(day,**kwargs)
         self.name = 'Tribal Immunity'
@@ -60,13 +108,3 @@ class TribalImmunity(TribesMixin,Challenge):
         going_to_tribal = [x for x in game.tribes if x.immunity == False][0]
         upcoming_tribal = list(filter(lambda x: not x.complete,game.schedule.event_type(TribalCouncil)))[0]
         upcoming_tribal.name = upcoming_tribal.name + ' ({})'.format(going_to_tribal.name)
-
-class TribalReward(TribesMixin,Challenge):
-    def __init__(self,day,**kwargs):
-        super().__init__(day,**kwargs)
-        self.name = 'Tribal Reward'
-
-    def end(self):
-        self.record('{} wins reward!'.format(self.result))
-        losing_tribes = list(set(self.who) - set([self.result]))
-        self.record('{}, head back to camp; got nothin\' for ya.'.format(losing_tribes))
--- events/events.py
+++ events/events.py
@@ -1,26 +1,24 @@
-from random import randint,choice,sample,shuffle
-from itertools import cycle
-
 
 class Event():
 
     eventId = 0
 
-    def __init__(self,day,time=12,schedule=None):
+    def __init__(self,day,time=12):
+
         self.id = Event.eventId
         Event.eventId += 1
+
         self.day = day
         self.time = time
         self.name = 'Event'
         self.complete = False
+
         self.num_players = None
         self.num_tribes = None
-        self.log = []
-        self.result = None
-        self.who = None
 
-        if schedule:
-            schedule.events.append(self)
+        self.log = []
+        self.participants = None
+        self.result = None
 
     def timestamp(self):
         return self.day + (self.time/100)
@@ -31,31 +29,12 @@ class Event():
     def record(self,string):
         self.log.append(string)
 
-    def find_participants(self,game):
-        self.who = game.players #by default
-
     def run(self,game):
-        self.find_participants(game)
-        self.record('{} - {} with {}.'.format(self.name,self.timestamp(),self.who))
-        self.start()
-        self.middle()
-        self.end()
-        self.game_changes(game)
+        self.mark_complete()
+        self.say_goodbye(game)
+
+    def mark_complete(self):
         self.complete = True
-        try:
-            game.day = game.get_next_event().day
-        except:
-            pass
 
-    def start(self):
-        pass
-
-    def middle(self):
-        self.result = choice(self.who)
-        self.record('The event selected {}.'.format(self.result))
-
-    def end(self):
-        pass
-
-    def game_changes(self,game):
+    def say_goodbye(self,game):
         pass
